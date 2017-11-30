67348c30f9a6255d33000e22408ee3734b1e1e48
Isaac Cook <isaac@simpload.com>
2017-11-29 18:00:42 -0600

    Name change

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-type JSONDec struct {
+type Decimal struct {
 	decimal.Decimal
 }
 
-func (t JSONDec) MarshalJSON() ([]byte, error) {
+func (t Decimal) MarshalJSON() ([]byte, error) {
 	return []byte("\"" + t.StringFixed(8) + "\""), nil
 }
 
-func (t *JSONDec) UnmarshalJSON(b []byte) error {
+func (t *Decimal) UnmarshalJSON(b []byte) error {
 	s := string(b)
 	if len(s) > 0 && s[0] == '"' {
 		s = s[1:]
@@ -24,24 +24,24 @@ func (t *JSONDec) UnmarshalJSON(b []byte) error {
 	}
 	dec, err := decimal.NewFromString(s)
 	if err != nil {
-		return errors.New("Incompatible type for JSONDec")
+		return errors.New("Incompatible type for Decimal")
 	}
-	*t = JSONDec{dec}
+	*t = Decimal{dec}
 	return nil
 }
 
-func (t JSONDec) Value() (driver.Value, error) {
+func (t Decimal) Value() (driver.Value, error) {
 	return []byte(t.String()), nil
 }
 
-func (s *JSONDec) Scan(src interface{}) error {
+func (s *Decimal) Scan(src interface{}) error {
 	switch src.(type) {
 	case []byte:
 		newDec, err := decimal.NewFromString(string(src.([]byte)))
 		if err != nil {
 			return err
 		}
-		*s = JSONDec{newDec}
+		*s = Decimal{newDec}
 	default:
 		return errors.New("Incompatible type for SDecimal")
 	}
